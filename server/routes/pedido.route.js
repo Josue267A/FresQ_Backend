4905340d836f9cc48b3c3a1aaef5d64ceed9f7c3
DaniAlej <168309704+daniAlej@users.noreply.github.com>
2024-07-27 21:25:42 -0500

    Consumido la base de datos hasta  confirmacion
    Se consume toda la base de datos hasta la confirmacion del pedido

--- server/routes/pedido.route.js
+++ server/routes/pedido.route.js
@@ -3,4 +3,6 @@ module.exports = function(app){
     app.get('/pedidosActivos/:idLocales', PedidoController.getPedidosActivos);
     app.get('/pedidosPasados/:idLocales',PedidoController.getPedidosPasados);
     app.put('/pedidoEstado/:idPedido',PedidoController.updatePedidoStatus);
+    app.post('/pedidos',PedidoController.createPedido);
+    app.get('/pedidos/:idPedido',PedidoController.getPedido);
 }
